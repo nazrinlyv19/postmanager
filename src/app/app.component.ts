64bd15815443c9bf311edafe9a1ea3f6d83efdc5
Nazrin E. Aliyeva <NEAliyeva@bankofbaku.com>
2025-06-30 15:45:33 +0400

    form add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,58 +1,42 @@
-// import { Component } from '@angular/core';
-// import { RouterOutlet } from '@angular/router';
-
-// @Component({
-//   selector: 'app-root',
-//   imports: [RouterOutlet],
-//   templateUrl: './app.component.html',
-//   styleUrl: './app.component.css'
-// })
-// export class AppComponent {
-//   title = 'blog-post-manager';
-// }
-
-
-
-
-
-import { Component, OnInit } from '@angular/core';
-import { PostService } from './services/post.service';
-import { Post } from './models/post.model';
+import { Component } from '@angular/core';
+import { FormsModule, FormControl, ReactiveFormsModule, FormGroup, Validator, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone:true,
-  template: `<h2>Test gedir... Konsola bax 😎</h2>`,
+  imports: [FormsModule, ReactiveFormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
-  constructor(private postService: PostService) {}
+export class AppComponent {
+  title:string = 'blog-post-manager';
+   userForm =new FormGroup({
+   fName: new FormControl('', Validators.required),
+   email:new FormControl(),
+   address:new FormControl(),
+   });
 
-  ngOnInit() {
-    // 1. Post əlavə edirik
-    this.postService.addPost({
-      id: 0, // ID sonradan servis tərəfindən təyin olunur
-      title: 'Test Post',
-      content: 'Bu test məzmundur',
-      optionalData: 'Nümunə'
-    });
+   
 
-    // 2. Bütün postları konsola yazırıq
-    console.log('📦 Əlavə olunmuş postlar:', this.postService.getPosts());
+   constructor(){
+    // console.log(this.fName)
+   }
 
-    // 3. Postu yeniləyirik
-    const oldPost = this.postService.getPosts()[0];
-    const updatedPost: Post = {
-      ...oldPost,
-      content: 'Bu məzmun dəyişdirildi!'
-    };
-    this.postService.updatePost(updatedPost);
-
-    console.log('✏️ Dəyişdirilmiş post:', this.postService.getPosts());
-
-    // 4. Postu silirik
-    this.postService.deletePost(oldPost.id);
-
-    console.log('❌ Silinmiş vəziyyət:', this.postService.getPosts());
+  formSubmit(event:any){
+    console.log('form submitted'); 
+    console.log(event);
   }
-}
+  
+  
+  getValue(fullName:any){
+  console.log(fullName);
+  }
 
+  onSubmitR(){
+    console.log(this.userForm.get('fName'));
+  }
+
+
+
+
+}
