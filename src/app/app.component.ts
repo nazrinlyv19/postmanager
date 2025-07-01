36a99f3ab20604d9b94f331003e1e966bae7bcae
Nazrin E. Aliyeva <NEAliyeva@bankofbaku.com>
2025-07-01 17:30:26 +0400

    formbuilder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,47 +1,34 @@
 import { Component } from '@angular/core';
-import { FormsModule, FormControl, ReactiveFormsModule, FormGroup, Validator, Validators} from '@angular/forms';
+import {
+  FormsModule,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+  FormArray,
+  FormBuilder
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  standalone:true,
+  standalone: true,
   imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title:string = 'blog-post-manager';
-   userForm =new FormGroup({
-   fName: new FormControl('',[ Validators.required,Validators.minLength(3)]),
-   email:new FormControl('',[Validators.required,Validators.email]),
-   address:new FormGroup({
-    street:new FormControl('',[Validators.required,Validators.minLength(3)]),
-    city:new FormControl('',[Validators.required,Validators.minLength(3)]),
-    zipCode:new FormControl('',[Validators.required,Validators.minLength(3)]),
-    country:new FormControl('',[Validators.required,Validators.minLength(3)])
+export class AppComponent  {
+  userForm!: FormGroup;
+  title: string = 'blog-post-manager';
+
+  constructor(private formBuilder: FormBuilder)
+   {
+ 
+}
+
+ngOnInit() {
+ this.userForm = this.formBuilder.group({
+    fName:['jack'],
+    password:[''],
    })
-   });
-
-   
-
-   constructor(){
-    // console.log(this.fName)
-   }
-
-  formSubmit(event:any){
-    console.log('form submitted'); 
-    console.log(event);
-  }
-  
-  
-  getValue(fullName:any){
-  console.log(fullName);
-  }
-
-  onSubmitR(){
-    console.log(this.userForm.get('fName'));
-  }
-
-
-
-
+}
 }
